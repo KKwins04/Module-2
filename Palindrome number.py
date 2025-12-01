3ef95064a1a66c4f88fe5af2eb7b244900d59c10
KKwins04 <arvind_kavin_karthik@students.edu.sg>
2025-12-01 16:34:04 +0800

    Palindrome commit

---
+++ Palindrome number.py
@@ -0,0 +1,15 @@
+#Take input from user
+num = int(input("Enter a number: "))
+
+rev = 0
+
+temp = num
+while temp > 0:
+    rem = temp % 10
+    rev = rem + (rev*10)
+    temp = int(temp/10)
+
+if rev==num:
+    print(num, "is an palindrome number")
+else:
+    print(num, "is not an palindrome number")
